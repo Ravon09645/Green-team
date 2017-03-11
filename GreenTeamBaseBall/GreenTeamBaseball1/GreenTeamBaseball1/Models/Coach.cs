eca5fdbdc8efb9f3c127831ae163f686c7ce4c62
Ravon09645 <Ravonrichardson@outlook.com>
2017-03-10 20:16:20 -0500

    updated the user interface

---
+++ GreenTeamBaseBall/GreenTeamBaseball1/GreenTeamBaseball1/Models/Coach.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GreenTeamBaseball1.Models;
+
+namespace GreenTeamBaseball1.Models
+{
+    public class Coach
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ GreenTeamBaseBall/GreenTeamBaseball1/GreenTeamBaseball1/ViewModels/BaseballViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GreenTeamBaseball1.Models;
+
+namespace GreenTeamBaseball1.ViewModels
+{
+    public class BaseballViewModel
+    {
+        public Baseball Baseball { get; set; }
+        public List<Coach> Coach { get; set; }
+    }
+}
